cf9f02ec21388c4a7a87b4083fed22b00ae99094
Whosmomo <pradiptaadinugroho@gmail.com>
2024-09-21 20:01:24 +0700

    menghapus test case

--- main/tests.py
+++ main/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase, Client
 from django.utils import timezone
-from .models import MoodEntry
+
 
 class mainTest(TestCase):
     def test_main_url_is_exist(self):
@@ -11,6 +11,6 @@ class mainTest(TestCase):
         response = Client().get('')
         self.assertTemplateUsed(response, 'main.html')
 
-    def test_nonexistent_page(self):
-        response = Client().get('/skibidi/')
-        self.assertEqual(response.status_code, 404)
+    # def test_nonexistent_page(self):
+    #     response = Client().get('/skibidi/')
+    #     self.assertEqual(response.status_code, 404)
